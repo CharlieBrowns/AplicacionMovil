37a93f508ebb61e3f9fe92d33bd056100956cef6
Romario <romariodiazholgado@gmail.com>
2020-01-05 19:00:55 -0500

    asdasd

--- app/models/vehiculo.js
+++ app/models/vehiculo.js
@@ -5,11 +5,11 @@ const Mongoose = require('mongoose')
 // se me van todos a ña mierda
 
 const Vehiculo = new Mongoose.Schema({
-    Dni_Propietario : {type : Number, required : true},
-    Dni_Infractor : {type : Number, required : true},
+    Dni_Propietario : {type : String, required : true},
+    Dni_Infractor : {type : String, required : true},
     Fecha_Ingreso : {type : String, required : true},
     Fecha_Salida : {type : String, required : true},
-    Nro_Tarjeta_Propiedad : {type : Number, required : true},
+    Nro_Tarjeta_Propiedad : {type : String, required : true},
     Modalidad_Servicio : {type : String, required : true},
     Tipo_Servicio : {type : String, required : true},
     Nro_Placa : {type : String, required : true},
